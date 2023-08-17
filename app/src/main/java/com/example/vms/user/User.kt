682db83197bc97a61f3d1545b423bc27581b3bf1
MichalSmiech <michal.smiech1@gmail.com>
2023-08-17 11:46:42 +0200

    Setup dagger (#2)
    Co-authored-by: Michał Śmiech <michal.smiech@mlabs.pl>

---
+++ app/src/main/java/com/example/vms/user/User.kt
@@ -0,0 +1,9 @@
+package com.example.vms.user
+
+/**
+ * Created by mśmiech on 06.08.2023.
+ */
+class User(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/example/vms/user/UserManager.kt
@@ -0,0 +1,24 @@
+package com.example.vms.user
+
+import com.example.vms.di.UserComponent
+import com.example.vms.di.UserModule
+
+/**
+ * Created by mśmiech on 06.08.2023.
+ */
+class UserManager(
+    private val userComponentBuilder: UserComponent.Builder
+) {
+    var userComponent: UserComponent? = null
+        private set
+
+    suspend fun startUserSession(user: User) {
+        userComponent = userComponentBuilder
+            .userModule(UserModule(user))
+            .build()
+    }
+
+    fun closeUserSession() {
+        userComponent = null
+    }
+}
