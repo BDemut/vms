fc20d795d27dba67ad908976f9b69d8306ee9aff
Michał Śmiech <michal.smiech@mlabs.pl>
2023-08-24 13:37:33 +0200

    Commit

--- app/src/main/java/com/example/vms/editvisit/TitleSection.kt
+++ app/src/main/java/com/example/vms/editvisit/TitleSection.kt
@@ -12,19 +12,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.ColorUtils
 import com.example.vms.R
 
 /**
