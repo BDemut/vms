f30a61557d4ee3e2bc291bb173c3d3c52b745a86
Michał Śmiech <michal.smiech@mlabs.pl>
2023-08-28 19:11:28 +0200

    Refactor LoginActivity

--- app/src/main/java/com/example/vms/login/LoginActivity.kt
+++ app/src/main/java/com/example/vms/login/LoginActivity.kt
@@ -6,13 +6,32 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -79,33 +98,60 @@ fun LoginScreen(
             .fillMaxSize(),
         color = MaterialTheme.colors.background,
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(24.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Username(state.username, { viewModel.setUsername(it) }, !state.displayValidErrors || state.isUsernameValid)
-            Spacer(modifier = Modifier.height(8.dp))
-            Password(state.password, { viewModel.setPassword(it) }, !state.displayValidErrors || state.isPasswordValid)
-            TextButton(onClick = viewModel::onLoginButtonClicked) {
-                Text(stringResource(R.string.login))
-            }
-        }
+        LoginContent(state = state,
+            onUsernameChange = { viewModel.setUsername(it) },
+            onPasswordChange = { viewModel.setPassword(it) },
+            onLoginButtonClicked = { viewModel.onLoginButtonClicked() }
+        )
         if (state.isLoading) {
-            Surface(
-                modifier = Modifier
-                    .fillMaxSize(),
-                color = Color(0, 0, 0, 120)
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    LoadingIndicator()
-                }
-            }
+            LoadingView()
+        }
+    }
+}
+
+@Composable
+fun LoadingView() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = Color(0, 0, 0, 120)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            LoadingIndicator()
+        }
+    }
+}
+
+@Composable
+fun LoginContent(
+    state: LoginState,
+    onUsernameChange: (String) -> Unit,
+    onPasswordChange: (String) -> Unit,
+    onLoginButtonClicked: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Username(
+            username = state.username,
+            onUsernameChange = onUsernameChange,
+            isUsernameValid = !state.displayValidErrors || state.isUsernameValid
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Password(
+            password = state.password,
+            onPasswordChange = onPasswordChange,
+            isPasswordValid = !state.displayValidErrors || state.isPasswordValid
+        )
+        TextButton(onClick = onLoginButtonClicked) {
+            Text(stringResource(R.string.login))
         }
     }
 }
