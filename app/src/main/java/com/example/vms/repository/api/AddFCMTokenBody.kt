6aed1e87dd8f25d6d94007e089cded21a5282486
Michał Śmiech <michal.smiech@mlabs.pl>
2023-09-28 14:32:54 +0200

    Handle adding fcm token request

--- app/src/main/java/com/example/vms/repository/api/AddFCMTokenBody.kt
+++ app/src/main/java/com/example/vms/repository/api/AddFCMTokenBody.kt
@@ -1,6 +1,9 @@
 package com.example.vms.repository.api
 
+import com.squareup.moshi.JsonClass
+
 /**
  * Created by mśmiech on 15.09.2023.
  */
+@JsonClass(generateAdapter = true)
 data class AddFCMTokenBody(val FCMToken: String)
